f4a73dc1abbf7224a19b6d7a20fd4f6ed32c7260
Josh <jzumbrunn@yahoo.com>
2023-08-02 08:14:11 -0400

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const routes = require("./routes");
+const { ValidationError } = require('sequelize');
 
 // Create a variable called isProduction that will be true if the environment is in production
 // or not by checking the environment key in the configuration file (backend/config/index.js):
@@ -59,5 +60,49 @@ app.use(
 
 // Connect all the routes
 app.use(routes);
+
+
+// The first error handler is actually just a regular middleware.
+//   It will catch any requests that don't match any of the routes defined and
+//   create a server error with a status code of 404.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+
+// The second error handler is for catching Sequelize errors and
+// formatting them before sending the error response.
+
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+
+
+// The last error handler is for formatting all the errors before returning a JSON response.
+//  It will include the error message, the error messages as a JSON object with key-value pairs,
+//  and the error stack trace (if the environment is in development) with the status code of the error message.
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 module.exports = app;
