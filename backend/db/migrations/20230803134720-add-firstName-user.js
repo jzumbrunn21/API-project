db2d5ca8c7d4b0954cbc114decd973e90e313922
Josh <jzumbrunn@yahoo.com>
2023-08-03 10:15:52 -0400

    Finalized backend, Ready to deploy

---
+++ backend/db/migrations/20230803134720-add-firstName-user.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("Users", "firstName", {
+      type: Sequelize.STRING,
+    });
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("Users", "firstName");
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
