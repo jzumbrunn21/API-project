6d1ab59f2a4d2d3e69e74fdf780e92ae2507b50f
Josh <jzumbrunn@yahoo.com>
2023-08-03 06:41:08 -0400

    Add User authentication Express middleware

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,10 +9,10 @@ router.use("/api", apiRouter);
 // In this test route, you are setting a cookie on the response with the name of XSRF-TOKEN
 //  to the value of the req.csrfToken method's return. Then, you are sending the text, Hello World! as the response's body.
 
-router.get("/hello/world", function (req, res) {
-  res.cookie("XSRF-TOKEN", req.csrfToken());
-  res.send("Hello World!");
-});
+// router.get("/hello/world", function (req, res) {
+//   res.cookie("XSRF-TOKEN", req.csrfToken());
+//   res.send("Hello World!");
+// });
 
 // Add a route, GET /api/csrf/restore to allow any developer to re-set the CSRF token cookie XSRF-TOKEN.
 router.get("/api/csrf/restore", (req, res) => {
@@ -24,6 +24,7 @@ router.get("/api/csrf/restore", (req, res) => {
 });
 // In this route, you are setting a cookie on the response with the name of XSRF-TOKEN to the value of the req.csrfToken method's return.
 // Then, send the token as the response for easy retrieval.
+
 
 
 
