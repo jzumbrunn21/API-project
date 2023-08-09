5ccf15f05e23c795400877aca1ec8f38119dbb31
Josh <jzumbrunn@yahoo.com>
2023-08-09 16:03:18 -0400

    commit in delete spot branch

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const { setTokenCookie, requireAuth } = require("../../utils/auth");
-const { Spot } = require("../../db/models");
+const {
+  setTokenCookie,
+  requireAuth,
+  restoreUser,
+} = require("../../utils/auth");
+const { Spot, User } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
+
+// DELETE SPOT BY ID //
+router.delete("/:spotId", requireAuth, async (req, res) => {
+  const deletedSpot = await Spot.findByPk(req.params.spotId);
+
+  if (deletedSpot && (this.owner === User.id)) {
+    await deletedSpot.destroy();
+    return res.json({
+      message: "Successfully Deleted",
+    });
+  } else {
+    res.status(404).json({ message: "Spot couldn't be found" });
+  }
+});
 
 // GET ALL SPOTS //
 router.get("", async (req, res) => {
