4b8c1f2e132de0ee5a22b3d50dd54073a00e32ed
Josh <jzumbrunn@yahoo.com>
2023-08-10 19:52:18 -0400

    delete review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -10,5 +10,19 @@ const { Spot, User, SpotImage, Review } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 
+router.delete("/:reviewId", requireAuth, async (req, res) => {
+  const deletedReview = await Review.findByPk(req.params.reviewId);
+
+  if (!deletedReview) {
+    return res.status(404).json({ message: "Review couldn't be found" });
+  }
+
+  if (deletedReview.userId !== req.user.id) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+
+  await deletedReview.destroy();
+  return res.json({ message: "Successfully deleted" });
+});
 
 module.exports = router;
