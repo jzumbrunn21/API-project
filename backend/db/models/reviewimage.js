68127e68d0f42d06309bc904f5462e41b0af6e1e
Josh <jzumbrunn@yahoo.com>
2023-08-13 13:30:53 -0400

    bug fix

--- backend/db/models/reviewimage.js
+++ backend/db/models/reviewimage.js
@@ -22,6 +22,9 @@ module.exports = (sequelize, DataTypes) => {
       url: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          isUrl: true,
+        },
       },
     },
     {
