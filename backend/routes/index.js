fb6f3347556c9f252f4ee5e9e41c84e8f5587d1d
Josh <jzumbrunn@yahoo.com>
2023-08-25 08:06:53 -0400

    sending to render

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,23 +1,19 @@
-// In this file, create an Express router, create a test route, and export the router at the bottom of the file.
-
-const express = require("express");
-const router = express.Router();
-const apiRouter = require("./api");
-
-router.use("/api", apiRouter);
-
 // backend/routes/index.js
-// ... after `router.use('/api', apiRouter);`
+const express = require('express');
+const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 // Static routes
 // Serve React build files in production
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
   // Serve the frontend's index.html file at the root route
-  router.get("/", (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(
-      path.resolve(__dirname, "../../frontend", "build", "index.html")
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 
@@ -26,38 +22,19 @@ if (process.env.NODE_ENV === "production") {
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(
-      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 }
 
 // Add a XSRF-TOKEN cookie in development
-if (process.env.NODE_ENV !== "production") {
-  router.get("/api/csrf/restore", (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.json({});
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.status(201).json({});
   });
 }
-
-// In this test route, you are setting a cookie on the response with the name of XSRF-TOKEN
-//  to the value of the req.csrfToken method's return. Then, you are sending the text, Hello World! as the response's body.
-
-// router.get("/hello/world", function (req, res) {
-//   res.cookie("XSRF-TOKEN", req.csrfToken());
-//   res.send("Hello World!");
-// });
-
-// Add a route, GET /api/csrf/restore to allow any developer to re-set the CSRF token cookie XSRF-TOKEN.
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    "XSRF-Token": csrfToken,
-  });
-});
-// In this route, you are setting a cookie on the response with the name of XSRF-TOKEN to the value of the req.csrfToken method's return.
-// Then, send the token as the response for easy retrieval.
 
 module.exports = router;
