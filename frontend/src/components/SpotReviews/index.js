6586cb33527a392ccbcf5f4c6c2ee6b1c2581b8c
Josh <jzumbrunn@yahoo.com>
2023-08-31 13:38:22 -0400

    get reviews

--- frontend/src/components/SpotReviews/index.js
+++ frontend/src/components/SpotReviews/index.js
@@ -1,10 +1,32 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getAllReviews } from "../../store/reviews";
+import { getAllSpots } from "../../store/spots";
+import "./SpotReviews.css";
+
 function SpotReviews() {
+  const dispatch = useDispatch();
+  const { spotId } = useParams();
+  const reviewsList = useSelector((state) => Object.values(state.reviews.spot));
+  useEffect(() => {
+    dispatch(getAllReviews());
+  }, [dispatch]);
+
+  const spotList = useSelector((state) => Object.values(state.spots.allSpots));
+
+  useEffect(() => {
+    dispatch(getAllSpots());
+  }, [dispatch]);
   return (
     <>
-    <div className="line-break"></div>
-    
-      <h2> </h2>
-      <h2>Hello</h2>
+      <div className="line-break"></div>
+      <div className="review-ticker">
+        {/* {spotList.map(({ avgRating, userId }) => (
+          <p key={userId}>{avgRating} * _ reviews </p>
+        ))} */}
+        <h2>Review Ticker</h2>
+      </div>
     </>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,7 @@ import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
 import * as spotActions from "./store/spots";
+import * as reviewActions from "./store/reviews";
 
 const store = configureStore();
 
@@ -22,6 +23,7 @@ if (process.env.NODE_ENV !== "production") {
   window.store = store;
   window.sessionActions = sessionActions;
   window.spotActions = spotActions;
+  window.reviewActions = reviewActions;
 }
 
 // Wrap the application with the Modal provider and render the Modal component
