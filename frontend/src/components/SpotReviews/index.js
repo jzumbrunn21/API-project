568e497eb8f7c3d9f50a407e683796829de2f09a
Josh <jzumbrunn@yahoo.com>
2023-08-31 13:47:49 -0400

    get revies

--- frontend/src/components/SpotReviews/index.js
+++ frontend/src/components/SpotReviews/index.js
@@ -9,24 +9,24 @@ function SpotReviews() {
   const dispatch = useDispatch();
   const { spotId } = useParams();
   const reviewsList = useSelector((state) => Object.values(state.reviews.spot));
-  useEffect(() => {
-    dispatch(getAllReviews());
-  }, [dispatch]);
+//   useEffect(() => {
+//     dispatch(getAllReviews());
+//   }, [dispatch]);
 
   const spotList = useSelector((state) => Object.values(state.spots.allSpots));
 
-  useEffect(() => {
-    dispatch(getAllSpots());
-  }, [dispatch]);
+//   useEffect(() => {
+//     dispatch(getAllSpots());
+//   }, [dispatch]);
   return (
     <>
       <div className="line-break"></div>
-      <div className="review-ticker">
-        {/* {spotList.map(({ avgRating, userId }) => (
+      {/* <div className="review-ticker">
+        {spotList.map(({ avgRating, userId }) => (
           <p key={userId}>{avgRating} * _ reviews </p>
-        ))} */}
+        ))}
         <h2>Review Ticker</h2>
-      </div>
+      </div> */}
     </>
   );
 }
