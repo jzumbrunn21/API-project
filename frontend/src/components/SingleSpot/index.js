c64463ce11450380a3cf6048385e0ff8ca552c7b
Josh <jzumbrunn@yahoo.com>
2023-09-27 13:34:23 -0400

    review count fix

--- frontend/src/components/SingleSpot/index.js
+++ frontend/src/components/SingleSpot/index.js
@@ -48,98 +48,108 @@ function SingleSpot() {
   }
   return (
     <>
-    <div className="view-single-spot">
-      <h2>{name}</h2>
-      <h4>
-        {city}, {state}, {country}
-      </h4>
-      <div className="image-container">
-        {SpotImages &&
-          SpotImages.length > 0 &&
-          SpotImages.map(({ id, url }) => (
-            <div key={id} className="spot-image">
-              <img src={url} alt={`Spot ${id}`} />
-            </div>
-          ))}
-      </div>
-      <div className="reservation-container">
-        {Owner && (
-          <h3 id="name">
-            Hosted by {Owner.firstName} {Owner.lastName}
-          </h3>
-        )}
-        <p id="description">{description}</p>
-        <div className="reservation-details">
-          <p id="priceSpot">${price} night</p>
-          <div className="reviewsSpot">
-            <p id="review-ticker">
-              <i className="fa-solid fa-star"></i> {""}
+      <div className="view-single-spot">
+        <h2>{name}</h2>
+        <h4>
+          {city}, {state}, {country}
+        </h4>
+        <div className="image-container">
+          {SpotImages &&
+            SpotImages.length > 0 &&
+            SpotImages.map(({ id, url }) => (
+              <div key={id} className="spot-image">
+                <img src={url} alt={`Spot ${id}`} />
+              </div>
+            ))}
+        </div>
+        <div className="reservation-container">
+          {Owner && (
+            <h3 id="name">
+              Hosted by {Owner.firstName} {Owner.lastName}
+            </h3>
+          )}
+          <p id="description">{description}</p>
+          <div className="reservation-details">
+            <p id="priceSpot">${price} night</p>
+            <div className="reviewsSpot">
+              {/* <i className="fa-solid fa-star"></i> {""}
               {avgStarRating === null
                 ? "New"
                 : parseFloat(avgStarRating).toFixed(1)}{" "}
-              {""} • {""}{" "}
-              {numReviews === 1
-                ? `${numReviews} review`
-                : ` ${numReviews} reviews`}
-            </p>
+                {""} • {""}{" "}
+                {numReviews === 1
+                  ? `${numReviews} review`
+                : ` ${numReviews} reviews`} */}
+              <p id="review-ticker">
+                <i className="fa-solid fa-star"></i>
+                {avgStarRating === null
+                  ? "New"
+                  : numReviews === 1
+                  ? `${parseFloat(avgStarRating).toFixed(1)}
+               •  ${numReviews} review`
+                  : `${parseFloat(avgStarRating).toFixed(1)}
+               •  ${numReviews} reviews`}
+              </p>
+            </div>
+            <button id="reserve-button" onClick={handleReservationClick}>
+              Reserve
+            </button>
           </div>
-          <button id="reserve-button" onClick={handleReservationClick}>
-            Reserve
-          </button>
+        </div>
+        <div className="line-break"></div>
+        <div className="reviews-ticker">
+          {currentUser === null && numReviews > 0 ? (
+            <div className="review-container">
+              <p>
+                <i className="fa-solid fa-star"></i> {""}
+                {avgStarRating === null
+                  ? "New"
+                  : parseFloat(avgStarRating).toFixed(1)}{" "}
+                {""} • {""}{" "}
+                {numReviews === 1
+                  ? `${numReviews} review`
+                  : ` ${numReviews} reviews`}
+              </p>
+            </div>
+          ) : numReviews < 1 ? (
+            <div className="review-container">
+              <p>
+                <i className="fa-solid fa-star"></i>
+                {""} New
+              </p>
+              {!Owner ||
+              (currentUser !== null && currentUser.id !== Owner.id) ? (
+                <>
+                  <CreateReviewModal />
+                </>
+              ) : null}
+
+              <div>
+                <p>Be the first to post a review!</p>
+              </div>
+            </div>
+          ) : numReviews > 0 ? (
+            <div className="review-container">
+              <p>
+                <i className="fa-solid fa-star"></i> {""}
+                {avgStarRating === null
+                  ? "New"
+                  : parseFloat(avgStarRating).toFixed(1)}{" "}
+                {""} • {""}{" "}
+                {numReviews === 1
+                  ? `${numReviews} review`
+                  : ` ${numReviews} reviews`}
+              </p>
+              {!Owner ||
+              (currentUser !== null && currentUser.id !== Owner.id) ? (
+                <>
+                  <CreateReviewModal />
+                </>
+              ) : null}
+            </div>
+          ) : null}
         </div>
       </div>
-      <div className="line-break"></div>
-      <div className="reviews-ticker">
-        {currentUser === null && numReviews > 0 ? (
-          <div className="review-container">
-            <p>
-              <i className="fa-solid fa-star"></i> {""}
-              {avgStarRating === null
-                ? "New"
-                : parseFloat(avgStarRating).toFixed(1)}{" "}
-              {""} • {""}{" "}
-              {numReviews === 1
-                ? `${numReviews} review`
-                : ` ${numReviews} reviews`}
-            </p>
-          </div>
-        ) : numReviews < 1 ? (
-          <div className="review-container">
-            <p>
-              <i className="fa-solid fa-star"></i>
-              {""} New
-            </p>
-            {!Owner || (currentUser !== null && currentUser.id !== Owner.id) ? (
-              <>
-                <CreateReviewModal />
-              </>
-            ) : null}
-
-            <div>
-              <p>Be the first to post a review!</p>
-            </div>
-          </div>
-        ) : numReviews > 0 ? (
-          <div className="review-container">
-            <p>
-              <i className="fa-solid fa-star"></i> {""}
-              {avgStarRating === null
-                ? "New"
-                : parseFloat(avgStarRating).toFixed(1)}{" "}
-              {""} • {""}{" "}
-              {numReviews === 1
-                ? `${numReviews} review`
-                : ` ${numReviews} reviews`}
-            </p>
-            {!Owner || (currentUser !== null && currentUser.id !== Owner.id) ? (
-              <>
-                <CreateReviewModal />
-              </>
-            ) : null}
-          </div>
-        ) : null}
-      </div>
-    </div>
     </>
   );
 }
